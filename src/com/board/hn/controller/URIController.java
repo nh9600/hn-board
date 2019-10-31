3149c01f01c44dc959ea49bc8b38d9f820f1f30b
Administrator <Administrator@koitt03a-PC>
2019-10-31 21:10:11 +0900

    얼레

---
+++ src/com/board/hn/controller/URIController.java
@@ -0,0 +1,29 @@
+package com.board.hn.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class URIController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String PREFIX = "WEB-INF";
+	private static final String SUFFIX = ".jsp";
+	
+	private static String getForwardURI(HttpServletRequest request) {
+		return PREFIX + request.getRequestURI()+SUFFIX;
+	}
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(getForwardURI(request));
+		rd.forward(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
